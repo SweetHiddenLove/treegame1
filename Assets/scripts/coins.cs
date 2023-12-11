0c9efb3c9a9e866e8edecff01991ccea3314c27b
SweetHiddenLove <146846079+SweetHiddenLove@users.noreply.github.com>
2023-12-11 19:28:23 +0000

    commit

---
+++ Assets/scripts/coins.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coins : MonoBehaviour
+{
+    private void Ontrigger(Collider other)
+    {
+        
+
+       // if player
+    }
+}
